fcb82482876aded9f9a3a0c2cbe70b534dad166d
acorbat <acorbat@df.uba.ar>
2018-09-22 12:44:24 -0300

    Corrector can process background corrections and documentation is up to date.

--- img_manager/corrector.py
+++ img_manager/corrector.py
@@ -1,17 +1,31 @@
 import numpy as np
 import lmfit as lm
+import matplotlib.pyplot as plt
 
-from img_manager import oiffile as oif
-from img_manager import tifffile as tif
-
-from foci_finder import pipelines as pipe
 
 class Corrector(object):
-    """An image or stack image corrector for background and bleaching. Parameters for correction can be modified
-    accordingly."""
+    """An image or stack image corrector for background, bleaching and bleeding. Parameters for correction can be modified
+    and fit accordingly.
+
+    Attributes
+    ----------
+    bkg_model : lmfit.Model class
+        The exponential model used to fit the dark noise images
+    bkg_params : lmfit.Parameters class
+        The parameters for the background correction
+
+    Methods
+    -------
+    bkg_exponential(x, amplitude, characteristic_time, constant)
+        Exponential function to be used in  background fitting
+    subtract_background(self, stack, time_step)
+        Subtracts background using the saved parameters from a stack of images
+    find_bkg_correction(self, stack, time_step, pp=None)
+        Loads a dark image stack and fits and saves the background correction paramaters
+    """
 
     def __init__(self):
-        """Return an image corrector with some initial parameters for background and bleaching correction."""
+        """Return an image corrector with some initial parameters for background, bleaching and blleding correction."""
         # background
         self.bkg_model = lm.Model(self.bkg_exponential, independent_vars=['x'])
         self.bkg_params = self.bkg_model.make_params(amplitude=9.30909784,
@@ -20,13 +34,43 @@ class Corrector(object):
         self.bkg_params['amplitude'].set(min=0)
         self.bkg_params['characteristic_time'].set(min=0)
 
-    ## Background Correction
+    # Background Correction
+    #######################
     @staticmethod
     def bkg_exponential(x, amplitude, characteristic_time, constant):
+        """Exponential function to fit background increment in time series.
+
+        Parameters
+        ----------
+        x : list, numpy.array
+            timepoints of function evaluation
+        amplitude : float
+            Amplitude of background noise variation through time
+        characteristic_time : float
+            Characteristic time of background variation
+        constant : float
+            Background noise stable value for long measurements
+
+        Returns
+        -------
+        Depending on input, returns value, list or array of background noise for the input timepoints
+        """
         return -amplitude * np.exp(-x / characteristic_time) + constant
 
-
     def subtract_background(self, stack, time_step):
+        """Subtracts background from a stack, considering time steps between images and using saved background
+        parameters.
+
+        Parameters
+        ----------
+        stack : numpy.array
+            Time series of images to be corrected
+        time_step
+            Time step between acquired images
+        Returns
+        -------
+            Returns the corrected stack
+        """
         times = np.arange(0, time_step * len(stack), time_step)
         stack_corrected = stack.copy()
         background = self.bkg_model.eval(self.bkg_params, x=times)
@@ -35,22 +79,56 @@ class Corrector(object):
 
         return stack_corrected
 
+    def find_bkg_correction(self, stack, time_step, pp=None):
+        """Given a dark noise path, it loads, calculates mean per timepoint, fits the parameters and saves them. If pp
+        is given a PdfPages, images of fit are saved.
 
-    def find_bkg_correction(self, dark_img_dir):
-        dark_img = oif.OifFile(str(dark_img_dir))
+        If fit is not converging, you can amnually modify parameters with:
+            >>> corrector.bkg_params['constant'].set(value=40)
+            >>> corrector.bkg_params['amplitude'].set(value=10)
+            >>> corrector.bkg_params['characteristic_time'].set(value=20)
 
-        stack = dark_img.asarray()[0].astype(float)
-        stack = tif.transpose_axes(stack, 'ZTYX', asaxes='TZYX')
-
-        time_step = pipe.get_t_step(dark_img)
+        Parameters
+        ----------
+        stack : numpy.array
+            stack of dark noise images to find background (can be masked images)
+        time_step : float
+            Time step between acquired images
+        pp : PdfPages
+            Images of fitting are saved in this pdf
+        """
         times = np.arange(0, time_step * len(stack), time_step)
 
         means = []
         for frame in stack:
-            means.append(np.mean(frame.flatten()))
+            means.append(np.nanmean(frame.flatten()))
 
         result = self.bkg_model.fit(means, params=self.bkg_params, x=times)
-        self.bkg_params = result.best_values
+
+        for key in result.best_values.keys():
+            self.bkg_params[key].set(value=result.best_values[key])
+
+            # Save a pdf of applied correction
+        if pp is not None:
+            plt.plot(times, means, 'ob')
+            plt.plot(times, self.bkg_model.eval(self.bkg_params, x=times))
+            plt.xlabel('time (s)')
+            plt.ylabel('mean intensity (a.u.)')
+            pp.savefig()
+            plt.close()
+
+            corrected = self.subtract_background(stack, time_step)
+
+            means = []
+            for frame in corrected:
+                means.append(np.mean(frame.flatten()))
+
+            plt.plot(times, means, 'ob')
+            plt.xlabel('time (s)')
+            plt.ylabel('mean intensity (a.u.)')
+            pp.savefig()
+            plt.close()
+
 
     ## Bleaching Correction
 
