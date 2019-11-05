b33e84f54d0c851ea90d958cc361eda5a16b96ec
acorbat <acorbat@df.uba.ar>
2019-11-05 09:38:52 -0300

    Added documentation for channel correctors

--- img_manager/correctors/channel_correctors.py
+++ img_manager/correctors/channel_correctors.py
@@ -8,8 +8,35 @@ from img_manager import corrector as corr
 
 
 class BleedingCorrector(corr.GeneralCorrector):
+    """This bleeding corrector can find the bleed factor between two channels
+    and correct for it.
+
+    Attributes
+    ----------
+    bleed_mean : float
+        Bleeding factor between channels
+    bleed_error : float
+        Standard error of the mean of the estimated bleeding factor
+
+    Methods
+    -------
+    correct_bleeding(channel_1, channel_2)
+        Corrects bleeding from channel_1 into channel_2.
+    find_bleeding(list_channel_1, list_channel_2, pp=None)
+        Finds bleeding factor from two lists corresponding to values from
+        each channel.
+    correct(stack_source, stack_to_correct)
+        This function corrects bleeding from one stack source into another
+        stack_to_correct.
+    to_dict()
+        Returns a dictionary with the parameters.
+    load_from_dict(valuesdict)
+        Loads the parameters from a saved dictionary.
+    """
 
     def __init__(self):
+        self.corrector_species = 'BleedingCorrector'
+
         # bleeding
         self.bleed_mean = 0
         self.bleed_error = 0
@@ -17,7 +44,7 @@ class BleedingCorrector(corr.GeneralCorrector):
     # Bleeding Correction
     #####################
     def correct_bleeding(self, channel_1, channel_2):
-        """Correct bleeding in channel_1 into channel_2.
+        """Corrects bleeding from channel_1 into channel_2.
 
         Parameters
         ----------
@@ -33,18 +60,22 @@ class BleedingCorrector(corr.GeneralCorrector):
         return channel_2 - self.bleed_mean * channel_1
 
     def find_bleeding(self, list_channel_1, list_channel_2, pp=None):
-        """Finds bleeding factor from two lists corresponding to values from each channel. Mean of ratios is saved as
-        bleeding factor, while error of the mean is saved as its error. If pp is given a PdfPages, the a histogram of
-        ratios is saved.
+        """Finds bleeding factor from two lists corresponding to values from
+        each channel. Mean of ratios is saved as bleeding factor, while error
+        of the mean is saved as its error. If pp is given a PdfPages, the a
+        histogram of ratios is saved.
 
-        Lists of values may proceed from mean of labeled regions or lists of flattened pixels, etc.
+        Lists of values may proceed from mean of labeled regions or lists of
+        flattened pixels, etc.
 
         Parameters
         ----------
         list_channel_1 : list, numpy.array
-            one dimensional array or list of values corresponding to intensities from channel_1
+            one dimensional array or list of values corresponding to
+            intensities from channel_1
         list_channel_2 : list, numpy.array
-            one dimensional array or list of values corresponding to intensities from channel_2
+            one dimensional array or list of values corresponding to
+            intensities from channel_2
         pp : PdfPages
             Images of histograms are saved in this pdf
         """
@@ -65,32 +96,74 @@ class BleedingCorrector(corr.GeneralCorrector):
             plt.close()
 
     def correct(self, stack_source, stack_to_correct):
-        """This function subtracts background from stack. time_step attribute is used."""
+        """This function corrects bleeding from one stack source into another
+        stack_to_correct."""
         return self.correct_bleeding(stack_source, stack_to_correct)
 
     def to_dict(self):
-        """Returns an OrderedDict with the parameters."""
-        # TODO: test
-        return {'bleed_mean': self.bleed_mean,
+        """Returns a dictionary with the parameters."""
+        return {'corrector_species': 'BleedingCorrector',
+                'bleed_mean': self.bleed_mean,
                 'bleed_error': self.bleed_error}
 
     def load_from_dict(self, valuesdict):
-        """Loads the parameters from a saved OrderedDict"""
-        # TODO: test
+        """Loads the parameters from a saved dictionary."""
         self.bleed_mean = valuesdict['bleed_mean']
         self.bleed_error = valuesdict['bleed_error']
 
 
 class ShiftCorrector(corr.GeneralCorrector):
+    """Estimates the shift between two channels using the image registration
+    package (correlation) and corrects the shift of channels.
 
+    Attributes
+    ----------
+    tvec : tuple
+        vector to use as shift between channels
+
+    Methods
+    -------
+    find_shift(master_stack, stack_to_move)
+        Finds the necessary shift to correct shift_to_move in order to match
+        master stack.
+    correct(stack)
+        Shifts the given stack of images according to tvec.
+    to_dict()
+        Returns a dictionary with the parameters.
+    load_from_dict(parameter_dict)
+        Loads the parameters from a saved dictionary.
+    """
     def __init__(self, tvec=None):
+        self.corrector_species = 'ShiftCorrector'
         self.tvec = tvec
 
     def find_shift(self, master_stack, stack_to_move):
+        """Finds the necessary shift to correct shift_to_move in order to match
+        master stack.
+
+        Parameters
+        ----------
+        master_stack : numpy.ndarray 2D
+            Image to use as reference
+        stack_to_move : numpy.ndarray 2D
+            Image that is to be shifted to match the reference
+        """
         result = ird.translation(master_stack, stack_to_move)
         self.tvec = [int(this) for this in result['tvec']]
 
     def correct(self, stack):
+        """Shifts the given stack of images according to tvec.
+
+        Parameters
+        ----------
+        stack : numpy.ndarray
+            stack of images to be shifted
+
+        Returns
+        -------
+        stack : numpy.ndarray
+            Shifted stack of images
+        """
         if len(stack.shape) == 2:
             stack = stack[np.newaxis, :]
 
@@ -100,7 +173,10 @@ class ShiftCorrector(corr.GeneralCorrector):
         return stack
 
     def to_dict(self):
-        return {'tvec': self.tvec}
+        """Returns a dictionary with the parameters."""
+        return {'corrector_species': 'ShiftCorrector',
+                'tvec': self.tvec}
 
     def load_from_dict(self, parameter_dict):
+        """Loads the parameters from a saved dictionary."""
         self.tvec = parameter_dict['tvec']
