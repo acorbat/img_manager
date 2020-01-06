eb2c01e20d5fdbc14320fe6633b48ba4491e99c3
acorbat <acorbat@df.uba.ar>
2020-01-06 17:58:28 -0300

    pathfile package seems to be built in and cannot be imported

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Agustin Corbat',
     author_email='acorbat@df.uba.ar',
     description='Image IO and correction.',
-    install_requires=['numpy', 'pathfile', 'datetime',
+    install_requires=['numpy', 'datetime',
                       'matplotlib', 'lmfit', 'imreg_dft', 'tifffile',
                       'oiffile'],
     dependency_links=['https://github.com/maurosilber/cellment.git',
