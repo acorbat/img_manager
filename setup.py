ef07b4e0ffed4b899cff255e734fdef3d8378f7c
acorbat <acorbat@df.uba.ar>
2020-01-06 17:53:18 -0300

    Typo in setup requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Agustin Corbat',
     author_email='acorbat@df.uba.ar',
     description='Image IO and correction.',
-    install_requires=['numpy', 'collection', 'pathfile', 'datetime',
+    install_requires=['numpy', 'collections', 'pathfile', 'datetime',
                       'matplotlib', 'lmfit', 'imreg_dft', 'tifffile',
                       'oiffile'],
     dependency_links=['https://github.com/maurosilber/cellment.git',
