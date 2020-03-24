fdfdbb9c09f51589972536c8b1b86df91f34cb6d
acorbat <acorbat@df.uba.ar>
2020-03-24 19:18:57 -0300

    inits not working

--- img_manager/__init__.py
+++ img_manager/__init__.py
@@ -2,5 +2,3 @@ from . import correctors
 from .corrector import CorrectorArmy, Channel
 from .fv1000 import FV1000
 from .lsm880 import LSM880
-
-__all__ = ['correctors', 'CorrectorArmy', 'Channel', 'FV1000', 'LSM880']
