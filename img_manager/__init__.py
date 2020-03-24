ce14f92bd721db4c80158865f8f18d5efd992b23
acorbat <acorbat@df.uba.ar>
2020-03-24 19:11:22 -0300

    import things in init of package

--- img_manager/__init__.py
+++ img_manager/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ['correctors']
+from . import correctors
+from .corrector import CorrectorArmy, Channel
+from .fv1000 import FV1000
+from .lsm880 import LSM880
+
+__all__ = ['correctors', 'CorrectorArmy', 'Channel', 'FV1000', 'LSM880']
